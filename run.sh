a6bcc2268d0872fe6a19811be3ab000315dc3c07
Nithach Vassanapong <62130500043@mail.kmutt.ac.th>
2021-05-07 07:33:22 +0000

    Edit run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-kubectl delete namespace student43-bookinfo-dev
-kubectl delete namespace student43-bookinfo-uat
-kubectl delete namespace student43-bookinfo-prd
-kubectl create namespace student43-bookinfo-dev
-kubectl create namespace student43-bookinfo-uat
-kubectl create namespace student43-bookinfo-prd
-kubectl config set-context $(kubectl config current-context) --namespace=student43-bookinfo-dev
-kubectl create secret generic registry-bookinfo --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson
-kubectl create configmap bookinfo-dev-ratings-mongodb-initdb --from-file=databases/ratings_data.json --from-file=databases/script.sh
-helm install -f k8s/helm-values/values-bookinfo-dev-ratings-mongodb.yaml bookinfo-dev-ratings-mongodb bitnami/mongodb
+kubectl delete namespace student43-ratings-dev
+kubectl delete namespace student43-ratings-uat
+kubectl delete namespace student43-ratings-prd
+kubectl create namespace student43-ratings-dev
+kubectl create namespace student43-ratings-uat
+kubectl create namespace student43-ratings-prd
+kubectl config set-context $(kubectl config current-context) --namespace=student43-ratings-dev
+kubectl create secret generic registry-ratings --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson
+kubectl create configmap ratings-dev-ratings-mongodb-initdb --from-file=databases/ratings_data.json --from-file=databases/script.sh
+helm install -f k8s/helm-values/values-ratings-dev-ratings-mongodb.yaml ratings-dev-ratings-mongodb bitnami/mongodb
